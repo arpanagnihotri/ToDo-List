b41fd2b42dd0c705d2c082e76d6dc0d0284364ee
Arpan Agnihotri <arpanagnihotri22@gmail.com>
2021-06-06 17:33:10 +0530

    first commit

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,14 @@
+const TodoForm = ({ handleSubmit, todo, editId, setTodo }) => {
+  return (
+    <form className="todoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">{editId ? "Edit" : "Go"}</button>
+    </form>
+  );
+};
+
+export default TodoForm;
